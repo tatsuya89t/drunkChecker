4cf1271e97a700ddd821b2a93f34e5dba4987615
Tatsuya Yamaguchi <lab.stjack89@gmail.com>
2016-12-22 01:04:16 +0900

    first commit

---
+++ drunkChecker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  drunkChecker
+//
+//  Created by sawanolab on 2016/12/21.
+//  Copyright (c) 2016年 AIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
