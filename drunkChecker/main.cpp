91a0358d672e28205200b339be881cfc9403536b
b16726 <b16726bb@aitech.ac.jp>
2017-01-16 14:12:46 +0900

    背景画像を保存する機能を実装

--- drunkChecker/main.cpp
+++ drunkChecker/main.cpp
@@ -12,20 +12,33 @@
 using namespace cv;
 
 int main(int argc, const char * argv[]) {
-    //画像の入力
-    Mat frame, size;
+    //カメラ映像の表示用
+    Mat frame;
     //映像の宣言
     VideoCapture cap(0);
     // キャプチャのエラー処理
     if (!cap.isOpened()) return -1;
+    
+    Mat ground; //背景画像用
+    cap >> ground;  //初期画像の取得
     
     while (1) {
         // カメラ映像の取得
         cap >> frame;
         // 映像の表示
         imshow("Camera", frame);
-        // キー入力があれば終了
-        if (waitKey(30) >= 0) break;
+        
+        // キー入力
+        int key=waitKey(30);
+        //終了キー
+        if (key == 'q' || key == 'Q')   break;
+        
+        //背景画像の更新用ボタン
+        if(key == 's'){
+            ground=frame.clone();
+        }
+        //背景画像表示
+        imshow("background", ground);
     }
     
     return 0;
