22d1a0638032b4795c80ae4ddd936bdfe35d0375
b16726 <b16726bb@aitech.ac.jp>
2017-01-18 19:05:54 +0900

    人領域の座標を算出プログラムの実装

--- drunkChecker/camera_maeda.cpp
+++ drunkChecker/camera_maeda.cpp
@@ -16,6 +16,9 @@ Drunker camera(VideoCapture cap, Mat ground) {
     //グレースケール用
     Mat dst_f, dst_g;
     
+    //二値化
+    Mat bin_img;
+    
     // カメラ映像の取得
     cap >> frame;
     //結果画像
@@ -26,30 +29,64 @@ Drunker camera(VideoCapture cap, Mat ground) {
     // キャプチャのエラー処理
     if (!cap.isOpened()) return d;
     
-    unsigned char s_f, s_g; //色値
+    unsigned char s_f, s_g, ans; //色値
     
+    //フレーム画像と背景画像のグレースケール変換
     cvtColor(frame, dst_f,CV_BGR2GRAY);
     cvtColor(ground, dst_g,CV_BGR2GRAY);
     
+    //背景差分
     for(int y=0; y<frame.size().height; y++){
         for(int x=0; x<frame.size().width; x++){
             s_f = dst_f.at<unsigned char>(y, x); //カメラ画素値の取得
             s_g = dst_g.at<unsigned char>(y, x); //背景画素値の取得
-            if(s_f==s_g){
+            ans = abs(s_f - s_g);
+            if(ans<20){
                 s_f = 0;
             }
             
             result.at<unsigned char>(y, x) = s_f;
         }
     }
-        
+    
+    threshold(result, bin_img, 128 ,255, cv::THRESH_BINARY|cv::THRESH_OTSU);
+    
+    
+    //領域座標の算出
+    //人領域のx座標の最小値最大値
+    d.x_min = frame.size().width;
+    d.x_max = 0;
+    //人領域のy座標の最小値最大値
+    d.y_min = frame.size().height;
+    d.y_max = 0;
+    
+    for(int y=0; y<frame.size().height; y++){
+        for(int x=0; x<frame.size().width; x++){
+            s_f = bin_img.at<unsigned char>(y, x); //カメラ画素値の取得
+            if(s_f == 255){
+                if(x<d.x_min){
+                    d.x_min = x;
+                }
+                if(x>d.x_max){
+                    d.x_max = x;
+                }
+                if(y<d.y_min){
+                    d.y_min = y;
+                }
+                if(x>d.y_max){
+                    d.y_max = y;
+                }
+            }
+        }
+    }
+    
     //テスト確認用
     // 映像の表示
     imshow("Camera", frame);
     //背景画像表示
     imshow("background", ground);
     
-    imshow("result",result);
+    imshow("result",bin_img);
     
     d.result_img = result.clone();
     
--- drunkChecker/camera_maeda.h
+++ drunkChecker/camera_maeda.h
@@ -14,8 +14,8 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 typedef struct Drunker {
-    int x, y;
-    int w, h;
+    int x_min, x_max;   //人領域の横幅
+    int y_min, y_max;   //人領域の縦幅
     int flug;
     Mat result_img;
 };
